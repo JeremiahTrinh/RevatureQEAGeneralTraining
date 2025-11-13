dd486e0a4547d63f53f0c9b8ff840b6cdb70ea83
JeremiahTrinh <demiahtrinh725@gmail.com>
2025-11-13 15:36:19 -0600

    Modified P0/Employee scripts to include corresponding TODO annotations

--- P0/Employee/employeeApp.py
+++ P0/Employee/employeeApp.py
@@ -1,4 +1,12 @@
 # Originally created: 11/11/2025
 # Version: 0.0.0
 
+# TODO
+# [] Log in with my credentials so that I can securely access my expense reports.
+# [] Submit a new expense with details about amount and description so that I can request reimbursement or track spending.
+# [] View the status of my submitted expenses so that I know whether they are pending, approved, or denied.
+# [] Edit expenses that are still pending so that I can correct mistakes before they are reviewed.
+# [] Delete expenses that are still pending so that I can correct mistakes before they are reviewed.
+# [] History of all my approved and denied expenses so that I can track my financial activity over time.
+
 print("Hello World!")
--- P0/Employee/employeeDBManager.py
+++ P0/Employee/employeeDBManager.py
@@ -1,2 +1,15 @@
 # Originally created: 11/13/2025
 # Version: 0.0.0
+
+import sqlite3 as sql
+import pandas as pd
+
+db = '../expenses_database.db'
+
+# TODO
+# [] Submit a new expense with details about amount and description so that I can request reimbursement or track spending.
+# [] View the status of my submitted expenses so that I know whether they are pending, approved, or denied.
+# [] Edit expenses that are still pending so that I can correct mistakes before they are reviewed.
+# [] Delete expenses that are still pending so that I can correct mistakes before they are reviewed.
+# [] History of all my approved and denied expenses so that I can track my financial activity over time.
+
