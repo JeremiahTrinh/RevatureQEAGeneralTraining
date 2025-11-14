24e4d137e30b9b62b17f703ee7f4c3b687a8e407
JeremiahTrinh <demiahtrinh725@gmail.com>
2025-11-13 19:50:15 -0600

    Modified employeeApp.py to include skeleton for methods

--- P0/Employee/employeeApp.py
+++ P0/Employee/employeeApp.py
@@ -1,6 +1,8 @@
 # Originally created: 11/11/2025
 # Version: 0.0.0
 
+from Employee.employeeDBManager import *
+
 # TODO
 # [] Log in with my credentials so that I can securely access my expense reports.
 # [] Submit a new expense with details about amount and description so that I can request reimbursement or track spending.
@@ -9,4 +11,29 @@
 # [] Delete expenses that are still pending so that I can correct mistakes before they are reviewed.
 # [] History of all my approved and denied expenses so that I can track my financial activity over time.
 
-print("Hello World!")
+def login():
+    pass
+
+def submit():
+    pass
+
+def view_status():
+    pass
+
+def edit():
+    pass
+
+def delete():
+    pass
+
+def history():
+    pass
+
+def print_main_menu():
+    pass
+
+def get_user_input():
+    pass
+
+if __name__ == '__main__':
+    print("Hello World!")
