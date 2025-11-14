1132c796b6ab72d8b9d206fc2a12d3f14988cfaa
JeremiahTrinh <demiahtrinh725@gmail.com>
2025-11-14 13:09:30 -0600

    Added CalculatorTest.java to mavendemo1

--- Week2Project/Demos/JavaDemos/mavendemo1/test/com/revature/mvndemo/CalculatorTest.java
+++ Week2Project/Demos/JavaDemos/mavendemo1/test/com/revature/mvndemo/CalculatorTest.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+package com.revature.mvndemo;
+
+import org.junit.jupiter.api.*;
 
 public class CalculatorTest {
+    private static final Calculator calculator = new Calculator();
+
+    @Test
+    public void testAdd1() {
+        int num1 = 10;
+        int num2 = 20;
+        int result = calculator.add(num1, num2);
+        Assertions.assertEquals(30, result);
+    }
+
+    @Test
+    public void testAdd2() {
+        int num1 = 10;
+        int num2 = 20;
+        int result = calculator.add(num1, num2);
+        Assertions.assertNotEquals(25, result);
+    }
+
+    @Test
+    public void testSubtract1() {
+        int num1 = 60;
+        int num2 = 15;
+        int result = calculator.subtract(num1, num2);
+        Assertions.assertEquals(45, result);
+    }
 }
